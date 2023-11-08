bc09501e45e5ed07a63c66f50ff00513daf4d5e4
gPagio <guiguipagio@gmail.com>
2023-11-07 21:32:04 -0300

    feat: escrever consulta no arquivo

---
+++ Java/database-conexoes/src/main/java/br/com/guilhermepagio/experimentos/database/conexoes/EscrevendoConsultaArquivoTexto.java
@@ -0,0 +1,58 @@
+package br.com.guilhermepagio.experimentos.database.conexoes;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class EscrevendoConsultaArquivoTexto {
+
+    public static String espacamentoLinhas = "---------------------------------\n";
+
+    public static void main(String[] args) {
+
+        Locale ptBr = new Locale("pt", "BR");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        PrintWriter printWriter;
+        Connection con;
+        Statement statement;
+        ResultSet resultSet;
+        //String url = "jdbc:postgresql://192.168.137.245:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "10203040";
+
+        System.out.println("Hello world!");
+
+        try {
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Conexão bem sucedida");
+            System.out.println(espacamentoLinhas);
+            try {
+                printWriter = new PrintWriter("resultado_consulta.txt", "UTF-8");
+                statement = con.createStatement();
+                resultSet = statement.executeQuery("select nf.numero, nf.data_venda, inf.codigo_do_produto, tdp.nome_do_produto, inf.preco from notas_fiscais nf inner join itens_notas_fiscais inf on INF.numero = NF.numero inner join tabela_de_produtos tdp on tdp.codigo_do_produto = inf.codigo_do_produto where nf.numero = 150;");
+                while (resultSet.next()) {
+                    printWriter.println(("Nº Nota: " + resultSet.getInt(1)));
+                    printWriter.println(("Data Venda: " + dateFormat.format(resultSet.getDate(2))));
+                    printWriter.println(("Cód. Produto: " + resultSet.getInt(3)));
+                    printWriter.println(("Descrição Produto: " + resultSet.getString(4)));
+                    printWriter.println(("Preço:" + NumberFormat.getCurrencyInstance(ptBr).format(resultSet.getDouble(5))));
+                    printWriter.print((espacamentoLinhas));
+                }
+                printWriter.close();
+            } catch (Exception e) {
+                System.out.println("Consulta não executada pelo seguinte problema: " + e.getMessage());
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Conexao falhou pelo seguinte motivo: " + e.getMessage());
+        }
+        System.out.println("Fim do programa");
+    }
+}
--- Java/database-conexoes/src/main/java/br/com/guilhermepagio/experimentos/database/conexoes/ImprimindoConsultaTerminal.java
+++ Java/database-conexoes/src/main/java/br/com/guilhermepagio/experimentos/database/conexoes/ImprimindoConsultaTerminal.java
@@ -1,21 +1,26 @@
-package br.com.guilhermepagio.experimentos;
+package br.com.guilhermepagio.experimentos.database.conexoes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
-public class Main {
+public class ImprimindoConsultaTerminal {
 
     public static String espacamentoLinhas = "---------------------------------\n";
 
     public static void main(String[] args) {
 
+        Locale ptBr = new Locale("pt", "BR");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Connection con;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        String url = "jdbc:postgresql://192.168.137.245:5432/postgres";
-        //String url = "jdbc:postgresql://localhost:5432/postgres";
+        Statement statement;
+        ResultSet resultSet;
+        //String url = "jdbc:postgresql://192.168.137.245:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/postgres";
         String username = "postgres";
         String password = "10203040";
 
@@ -30,20 +35,21 @@ public class Main {
                 resultSet = statement.executeQuery("select nf.numero, nf.data_venda, inf.codigo_do_produto, tdp.nome_do_produto, inf.preco from notas_fiscais nf inner join itens_notas_fiscais inf on INF.numero = NF.numero inner join tabela_de_produtos tdp on tdp.codigo_do_produto = inf.codigo_do_produto where nf.numero = 100;");
                 while (resultSet.next()) {
                     System.out.println("Nº Nota: " + resultSet.getInt(1));
-                    System.out.println("Data Venda: " + resultSet.getDate(2));
+                    System.out.println("Data Venda: " + dateFormat.format(resultSet.getDate(2)));
                     System.out.println("Cód. Produto: " + resultSet.getInt(3));
                     System.out.println("Descrição Produto: " + resultSet.getString(4));
-                    System.out.println("Preço:" + resultSet.getDouble(5));
+                    System.out.println("Preço:" + NumberFormat.getCurrencyInstance(ptBr).format(resultSet.getDouble(5)));
                     System.out.println(espacamentoLinhas);
                     Thread.sleep(1000);
                 }
             } catch (Exception e) {
                 System.out.println("Consulta não executada pelo seguinte problema: " + e.getMessage());
+            } finally {
+                con.close();
             }
         } catch (Exception e) {
             System.out.println("Conexao falhou pelo seguinte motivo: " + e.getMessage());
         }
-
         System.out.println("Fim do programa");
     }
 }
